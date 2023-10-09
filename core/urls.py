3be910cccaa02820e305617c8ce22e1913092d91
the-iron-ryan <ryandougherty25@gmail.com>
2023-10-08 19:33:16 -0700

    Added template theme

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for the_iron_website project.
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('', include('projects.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path("admin/", admin.site.urls),
+    path("", include('theme_pixel.urls'))
 ]
