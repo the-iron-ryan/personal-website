0fb6e61381a8026484039aef7d09dd17a1b3940d
Ryan Dougherty <ryandougherty25@gmail.com>
2023-10-21 18:53:20 -0700

    Reverted the header logo back to its original design. Got lab and game projects up and running

--- home/models.py
+++ home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# ==================== #
+# Base Project Model
+# ==================== #
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(default="")
+    
+    class Meta:
+        # Make this an abstract class so that it wont be created in the database
+        abstract = True
+    
+
+class Lab(Project):
+    pdf_title = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'home_lab'
+    
+class Game(Project):
+    url = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'home_game'
+    
+
